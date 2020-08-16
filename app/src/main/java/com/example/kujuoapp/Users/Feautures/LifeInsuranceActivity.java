ffcbe8350385ccb76968a03e234a04b7a1e01740
ZeeshanFaisal10 <zeshanfaisal10@gmail.com>
2020-08-16 19:56:01 +0500

    added insurance screens

--- app/src/main/java/com/example/kujuoapp/Users/Feautures/LifeInsuranceActivity.java
+++ app/src/main/java/com/example/kujuoapp/Users/Feautures/LifeInsuranceActivity.java
@@ -2,15 +2,27 @@ package com.example.kujuoapp.Users.Feautures;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.kujuoapp.R;
 
 public class LifeInsuranceActivity extends AppCompatActivity {
 
+    LinearLayout termsandcond;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_insurance);
+        termsandcond = findViewById(R.id.tandc_l);
+
+        termsandcond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),PDFActivity.class));
+            }
+        });
     }
 }
